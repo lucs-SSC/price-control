b32f60d6c29d2273b6b271d19306475b314d136d
Lucas Corrêa <l.scorrea.01@gmail.com>
2025-06-11 21:16:00 -0300

    Creation of Field Validating

--- backend/src/controller/productPurchaseController/createProductPurchase.ts
+++ backend/src/controller/productPurchaseController/createProductPurchase.ts
@@ -1,54 +1,32 @@
 import { Request, Response } from 'express';
 import { prisma } from '../../../prisma/client';
+import { validateFields } from '../../utils/validateFields';
 
-import { validate as isUUID } from 'uuid';
 
 export const createProductPurchase = async(request: Request, response: Response) => {
     const { userId, productId, storeId, price } = request.body;
 
     const ids = { userId, productId, storeId };
 
-    const invalidFields: string[] = [];
-
-    for (const [key, value] of Object.entries(ids)){
-        if(value !== undefined && value !== null && value !== ''){
-            if(!isUUID(value)){
-                invalidFields.push(key);
-            }
-        }
-        
+    const { invalidFormat, notFound } = await validateFields({ userId, productId, storeId });
+    
+    if(invalidFormat.length > 0) {
+        response.status(400).json({error: `Invalid ID format for: ${invalidFormat.join(', ')}`});
+        return;
     }
 
-    if(invalidFields.length > 0){
-        response.status(400).json({
-           error: `Invalid ID format for: ${invalidFields.join(', ')}` ,
-        })
+   if(notFound.length > 0 ){
+        const errors = notFound.map(field => ({
+            field,
+            message: `${field} not found`,
+        }));
 
+        response.status(404).json({ errors });
         return;
-    };
+   }
 
     try{
 
-        const [user, product, store] = await Promise.all([
-            prisma.user.findUnique({ where: {id: userId}}),
-            prisma.product.findUnique({ where: {id: productId}}),
-            prisma.store.findUnique({ where: {id: storeId}}),
-        ]);
-
-        if(!user) invalidFields.push('userId');
-        if(!product) invalidFields.push('productId');
-        if(!store) invalidFields.push('storeId');
-
-        if(invalidFields.length > 0){
-            const errors = invalidFields.map(field => ({
-                field,
-                message: `${field} not found`,
-            }));
-
-            response.status(404).json({ errors });
-            return;
-        }
-        
         const createProductPurchase = await prisma.productPurchase.create({
             data: {
                 price,
---
+++ backend/src/utils/validateFields.ts
@@ -0,0 +1,33 @@
+import { validate as isUUID } from 'uuid';
+import { prisma } from '../../prisma/client';
+
+interface Fields {
+    [key: string]: string | undefined;
+}
+
+export const validateFields = async (ids: Fields) => {
+    const invalidFormat: string[] = [];
+    const notFound: string[] = [];
+
+    for (const [key, value] of Object.entries(ids)){
+        if(value && !isUUID(value)){
+            invalidFormat.push(key);
+        }
+    }
+
+    if(invalidFormat.length > 0){
+        return { invalidFormat, notFound};
+    }
+
+    const checks = await Promise.all([
+        ids.userId ? prisma.user.findUnique({ where: { id: ids.userId } }) : Promise.resolve(null),
+        ids.productId ? prisma.product.findUnique({ where: { id: ids.productId } }) : Promise.resolve(null),
+        ids.storeId ? prisma.store.findUnique({ where: { id: ids.storeId } }) : Promise.resolve(null),
+    ]);
+
+    if (ids.userId && !checks[0]) notFound.push('userId');
+    if (ids.productId && !checks[1]) notFound.push('productId');
+    if (ids.storeId && !checks[2]) notFound.push('storeId');
+
+    return { invalidFormat, notFound };
+}
